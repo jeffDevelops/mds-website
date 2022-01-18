4202a9aff52889998d029a78d4b98aadfc75ee61
Henry Quinn <henryquinniv@gmail.com>
2022-01-18 13:43:28 -0500

    Editing 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/Layout"
 import Seo from "../components/seo"
@@ -6,8 +7,9 @@ import Seo from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>404 Message</h1>
+    <h3>Page Not Found</h3>
+    <p>Sorry, but we can't seem to find the page you're looking for. Try going back to the previous page or visit our <Link to="/faq">FAQ page</Link> for more information.</p>
   </Layout>
 )
 
