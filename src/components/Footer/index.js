1cd6d51713c0a1bbf2a5bd6824dd5bc83e53d586
Henry Quinn <henryquinniv@gmail.com>
2022-01-18 14:53:40 -0500

    Reordering footer.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,25 +9,16 @@ import * as FooterStyles from "./footer.module.css"
 const Footer = () => (
   <div className={FooterStyles.footer}>
     <section className={FooterStyles.ftMain}>
-    <div className={FooterStyles.ftMainItem}>
-      <StaticImage
-        src="../../images/mayday-logo-circle.png"
-        width={300}
-        quality={95}
-        formats={["auto", "webp", "avif"]}
-        alt="May Day Strike Icon"
-      />
-    </div>
       <div className={FooterStyles.ftMainItem}>
-        <div className={FooterStyles.ftColumnContent}>
-          <h2 className={FooterStyles.ftTitle}>Connect With Us</h2>
-          <ul>
-            <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://discord.gg/7ScvDppASV" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faDiscord} /> Discord</a></li>
-            <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://www.reddit.com/r/MayDayStrike/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faRedditAlien} /> Reddit</a></li>
-            <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://twitter.com/MayDayStrike22" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter} /> Twitter</a></li>
-          </ul>
-        </div>
+        <StaticImage
+          src="../../images/mayday-logo-circle.png"
+          width={300}
+          quality={95}
+          formats={["auto", "webp", "avif"]}
+          alt="May Day Strike Icon"
+        />
       </div>
+      
       <div className={FooterStyles.ftMainItem}>
         <div className={FooterStyles.ftColumnContent}>
           <h2 className={FooterStyles.ftTitle}>Follow Us</h2>
@@ -35,6 +26,16 @@ const Footer = () => (
             <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://www.instagram.com/MayDayStrike/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} /> Instagram</a></li>
             <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://www.snapchat.com/add/MayDayStrike" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faSnapchatGhost} /> Snapchat</a></li>
             <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://www.youtube.com/channel/UC_oY2LvpK7LL36raH7J0_og" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faYoutube} /> YouTube</a></li>
+          </ul>
+        </div>
+      </div>
+      <div className={FooterStyles.ftMainItem}>
+        <div className={FooterStyles.ftColumnContent}>
+          <h2 className={FooterStyles.ftTitle}>Connect With Us</h2>
+          <ul>
+            <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://discord.gg/7ScvDppASV" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faDiscord} /> Discord</a></li>
+            <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://www.reddit.com/r/MayDayStrike/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faRedditAlien} /> Reddit</a></li>
+            <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://twitter.com/MayDayStrike22" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter} /> Twitter</a></li>
           </ul>
         </div>
       </div>
