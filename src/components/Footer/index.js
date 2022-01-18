a169175cf5fc6f6c92dc870b285614596d40023f
Henry Quinn <henryquinniv@gmail.com>
2022-01-18 13:44:47 -0500

    Messing with stylings to get links correct.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -21,9 +21,9 @@ const Footer = () => (
         <div className={FooterStyles.ftColumnContent}>
           <h2 className={FooterStyles.ftTitle}>Connect With Us</h2>
           <ul>
-            <li className={FooterStyles.socialsListItem}><a href="https://discord.gg/7ScvDppASV"><FontAwesomeIcon icon={faDiscord} /> Discord</a></li>
-            <li className={FooterStyles.socialsListItem}><a href="https://www.reddit.com/r/MayDayStrike/"><FontAwesomeIcon icon={faRedditAlien} /> Reddit</a></li>
-            <li className={FooterStyles.socialsListItem}><a href="https://twitter.com/MayDayStrike22"><FontAwesomeIcon icon={faTwitter} /> Twitter</a></li>
+            <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://discord.gg/7ScvDppASV"><FontAwesomeIcon icon={faDiscord} /> Discord</a></li>
+            <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://www.reddit.com/r/MayDayStrike/"><FontAwesomeIcon icon={faRedditAlien} /> Reddit</a></li>
+            <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://twitter.com/MayDayStrike22"><FontAwesomeIcon icon={faTwitter} /> Twitter</a></li>
           </ul>
         </div>
       </div>
@@ -31,9 +31,9 @@ const Footer = () => (
         <div className={FooterStyles.ftColumnContent}>
           <h2 className={FooterStyles.ftTitle}>Follow Us</h2>
           <ul>
-            <li className={FooterStyles.socialsListItem}><a href="https://www.instagram.com/MayDayStrike/"><FontAwesomeIcon icon={faInstagram} /> Instagram</a></li>
-            <li className={FooterStyles.socialsListItem}><a href="https://www.snapchat.com/add/MayDayStrike"><FontAwesomeIcon icon={faSnapchatGhost} /> Snapchat</a></li>
-            <li className={FooterStyles.socialsListItem}><a href="https://www.youtube.com/channel/UC_oY2LvpK7LL36raH7J0_og"><FontAwesomeIcon icon={faYoutube} /> YouTube</a></li>
+            <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://www.instagram.com/MayDayStrike/"><FontAwesomeIcon icon={faInstagram} /> Instagram</a></li>
+            <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://www.snapchat.com/add/MayDayStrike"><FontAwesomeIcon icon={faSnapchatGhost} /> Snapchat</a></li>
+            <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://www.youtube.com/channel/UC_oY2LvpK7LL36raH7J0_og"><FontAwesomeIcon icon={faYoutube} /> YouTube</a></li>
           </ul>
         </div>
       </div>
