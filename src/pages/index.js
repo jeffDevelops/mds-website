4798f4c0f6e64f35c3bf0b58e6e196da611495b3
Henry Quinn <henryquinniv@gmail.com>
2022-01-17 20:42:51 -0500

    Styling fix.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import DemandList from "../components/DemandList"
 const IndexPage = () => (
   <Layout>
     <Seo title="More Info Coming Soon" />
-    <h1>More Information Coming Soon</h1>
+    <h1>More Information Coming Soon!</h1>
     <p>People have had enough. The MayDay strike is a strike being held on the 1st of May lasting 2 weeks attempting to achieve global efforts by encouraging thousands, millions of people, celebrities and unions to stand up. On May 1st 2022 workers will either quit their jobs, go on strike or do what they can. Join the subreddit or Discord to discuss the day.</p>
     <DemandList />
   </Layout>
