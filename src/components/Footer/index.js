b3b866dc259669311a2f2256708574c02a175eab
Henry Quinn <henryquinniv@gmail.com>
2022-01-18 14:52:05 -0500

    Adding Volunteer form to footer.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import { StaticImage } from "gatsby-plugin-image"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faDiscord, faInstagram, faRedditAlien, faSnapchatGhost, faTwitter, faYoutube } from "@fortawesome/free-brands-svg-icons"
+import { faHandsHelping } from "@fortawesome/free-solid-svg-icons"
 
 import * as FooterStyles from "./footer.module.css"
 
@@ -21,9 +22,9 @@ const Footer = () => (
         <div className={FooterStyles.ftColumnContent}>
           <h2 className={FooterStyles.ftTitle}>Connect With Us</h2>
           <ul>
-            <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://discord.gg/7ScvDppASV"><FontAwesomeIcon icon={faDiscord} /> Discord</a></li>
-            <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://www.reddit.com/r/MayDayStrike/"><FontAwesomeIcon icon={faRedditAlien} /> Reddit</a></li>
-            <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://twitter.com/MayDayStrike22"><FontAwesomeIcon icon={faTwitter} /> Twitter</a></li>
+            <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://discord.gg/7ScvDppASV" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faDiscord} /> Discord</a></li>
+            <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://www.reddit.com/r/MayDayStrike/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faRedditAlien} /> Reddit</a></li>
+            <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://twitter.com/MayDayStrike22" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter} /> Twitter</a></li>
           </ul>
         </div>
       </div>
@@ -31,9 +32,17 @@ const Footer = () => (
         <div className={FooterStyles.ftColumnContent}>
           <h2 className={FooterStyles.ftTitle}>Follow Us</h2>
           <ul>
-            <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://www.instagram.com/MayDayStrike/"><FontAwesomeIcon icon={faInstagram} /> Instagram</a></li>
-            <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://www.snapchat.com/add/MayDayStrike"><FontAwesomeIcon icon={faSnapchatGhost} /> Snapchat</a></li>
-            <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://www.youtube.com/channel/UC_oY2LvpK7LL36raH7J0_og"><FontAwesomeIcon icon={faYoutube} /> YouTube</a></li>
+            <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://www.instagram.com/MayDayStrike/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} /> Instagram</a></li>
+            <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://www.snapchat.com/add/MayDayStrike" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faSnapchatGhost} /> Snapchat</a></li>
+            <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://www.youtube.com/channel/UC_oY2LvpK7LL36raH7J0_og" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faYoutube} /> YouTube</a></li>
+          </ul>
+        </div>
+      </div>
+      <div className={FooterStyles.ftMainItem}>
+        <div className={FooterStyles.ftColumnContent}>
+          <h2 className={FooterStyles.ftTitle}>Join Us</h2>
+          <ul>
+            <li className={FooterStyles.socialsListItem}><a className={FooterStyles.socialsListItemLink} href="https://docs.google.com/forms/d/e/1FAIpQLSeNzjMZdnnp8VC4_kPsSOGbXOcZ3LkOTWNm_RaWZKRwIN5E_g/viewform" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faHandsHelping} /> Volunteer</a></li>
           </ul>
         </div>
       </div>
