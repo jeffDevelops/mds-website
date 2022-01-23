1b7f301bf9d14c3280e45ba6e558bb94518f68c1
Henry Quinn <henryquinniv@gmail.com>
2022-01-22 23:38:22 -0500

    Adding MenuBar component.

---
+++ src/components/MenuBar/index.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import * as MenuBarStyles from "./menuBar.module.css"
+
+export default () => (
+  <div>
+    <ul className={MenuBarStyles.menuBarList}>
+      <li>
+        <Link className={MenuBarStyles.menuBarItemLink} to="/">Home</Link>
+      </li>
+      <li>
+        <Link className={MenuBarStyles.menuBarItemLink} to="/faq">FAQ</Link>
+      </li>
+      <li>
+        <Link className={MenuBarStyles.menuBarItemLink} to="/links">Links</Link>
+      </li>
+      <li>
+        <Link className={MenuBarStyles.menuBarItemLink} to="/">Home</Link>
+      </li>
+      <li>
+        <Link className={MenuBarStyles.menuBarItemLink} to="/faq">FAQ</Link>
+      </li>
+      <li>
+        <Link className={MenuBarStyles.menuBarItemLink} to="/links">Links</Link>
+      </li>
+      <li>
+        <Link className={MenuBarStyles.menuBarItemLink} to="/">Home</Link>
+      </li>
+      <li>
+        <Link className={MenuBarStyles.menuBarItemLink} to="/faq">FAQ</Link>
+      </li>
+      <li>
+        <Link className={MenuBarStyles.menuBarItemLink} to="/links">Links</Link>
+      </li>
+    </ul>
+  </div>
+)
