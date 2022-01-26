0bc8ab52c67c2d1cc279e000f3cca00cc5bf7d8a
Mags <4146037+MagsMagnoli@users.noreply.github.com>
2022-01-25 21:50:46 -0500

    fix responsive navbar

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -4,8 +4,18 @@ import { Link } from "gatsby"
 
 import * as MenuBarStyles from "./menuBar.module.css"
 
-// Thank you, @joshcawthorne!
-// https://gist.github.com/joshcawthorne/0a518b164658510f4eed74d0c4e8d003
+/**
+ * @typedef {Object} WindowSize
+ * @property {number} width - The window inner width in pixels
+ * @property {number} height - The window inner height in pixels
+ */
+
+/**
+ * Thank you, @joshcawthorne!
+ * https://gist.github.com/joshcawthorne/0a518b164658510f4eed74d0c4e8d003
+ * 
+ * @returns {WindowSize}
+ * */
 function useWindowSize() {
   const [windowSize, setWindowSize] = useState({
     width: undefined,
@@ -27,21 +37,22 @@ function useWindowSize() {
   return windowSize;
 }
 
+const mobileWidth = 600;
+
 const MenuBar = () => {
   const [isOpen, setOpen] = useState(true);
 
-  let width = useWindowSize();
-  // let width = window.innerWidth;
+  const { width } = useWindowSize();
 
   return (
     <nav>
       <div>
-        {width.width < 601 &&
+        {width <= mobileWidth &&
           <div className={MenuBarStyles.hamburger}>
             <Hamburger toggled={isOpen} toggle={setOpen} size={34} duration={0.5} color="#E9DFAC" label="Show MenuBar" />
           </div>
         }
-        {isOpen && (
+        {(width > mobileWidth || isOpen) && (
           <div>
             <ul className={MenuBarStyles.menuBarList}>
               <li>
